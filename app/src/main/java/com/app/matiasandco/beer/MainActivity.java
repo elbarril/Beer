9ed677f41ff438d2a93e59bf8ba621e3a953880a
eldaroo <darioegea@gmail.com>
2018-06-10 18:26:37 -0300

    Changes

--- app/src/main/java/com/app/matiasandco/beer/MainActivity.java
+++ app/src/main/java/com/app/matiasandco/beer/MainActivity.java
@@ -1,13 +1,27 @@
 package com.app.matiasandco.beer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button letsGo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    letsGo = findViewById(R.id.btn);
+
+    letsGo.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent next = new Intent(MainActivity.this,ScrollingActivity.class);
+            startActivity(next);
+        }
+    });
     }
 }
